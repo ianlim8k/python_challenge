b8e8a9bde77af920e018e76070c125476888b938
ianlim8k <65546562+ianlim8k@users.noreply.github.com>
2020-06-13 09:23:33 +1000

    Pybank main.py updated with comments

--- PyBank/main.py
+++ PyBank/main.py
@@ -21,9 +21,13 @@ with open(budget_csv, 'r') as csvfile:
     change_counter = 0
 
     for row in csvreader:
+        #Count the number of months
         months = months + 1
+
+        # calculate total profit
         profit_loss = int(row[1])
         total_pl = total_pl + profit_loss 
+        
         #calculate change in profit/loss and append to list pl_change
         if change_counter == 0:
             previous_pl = profit_loss
@@ -31,6 +35,7 @@ with open(budget_csv, 'r') as csvfile:
         else:
             change_pl = profit_loss - previous_pl
             profitloss_change.append(change_pl)
+            #Get the period and change in profit
             period.append(row[0])
             previous_pl = profit_loss
             change_counter += 1            
@@ -38,40 +43,27 @@ with open(budget_csv, 'r') as csvfile:
      #calculate average profit_loss change 
     average_change = round(sum(profitloss_change) / len(profitloss_change),2)
 
-    #Find the greatest increase in profit/loss
+    #Find the greatest increase and greatest decrease in profit/loss
     max_change = max(profitloss_change)
     max_position = profitloss_change.index(max(profitloss_change))
     max_period = period[max_position]
     min_change = min(profitloss_change)
     min_position = profitloss_change.index(min(profitloss_change))
     min_period = period[min_position]
-    # print(round(average_change,2))
-
-
-    # for change in pl_change: 
-    # print(pl_change)
-    # print(period)
-    # print(max_change) 
-    # print(fmax_position)  
-    # print(max_period)
-    # print(min_change)  
-   
+    
+    #Print result to Terminal
     print(f"The total number of months is {str(months)}")
     print(f"The total profit/loss is {str(total_pl)}")
     print(f"The average change is ${str(average_change)}")
     print(f"The greatest increase in profit is {max_period}, $({max_change}) ")
     print(f"The greatest decrease in losses is {min_period}, $({min_change}) ")
     
-    #calculate profit_loss change
-
-    # Specify the file to write to
+    # Write result to output file
+    # Specify the file and path
     output_path = os.path.join("Analysis", "Financial_Analysis.txt")
 
-    # Open the file using "write" mode. Specify the variable to hold the contentsgi
+    # Open the file using "write" mode. Specify the variable to hold the contents
     with open(output_path, 'w') as txtfile:
-
-    # # Initialize csv.writer
-    # txtwriter = txt.writer(txtfile, delimiter=',')
 
     # Write the first row (column headers)
         txtfile.write("Financial Analysis\n")
@@ -81,9 +73,7 @@ with open(budget_csv, 'r') as csvfile:
         txtfile.write("Average Change: $%s\n" % (average_change))
         txtfile.write("Greatest Increase in Profits: %s $(%s)\n" % (max_period,max_change))
         txtfile.write("Greatest descrease in Losses: %s $(%s)\n" % (min_period,min_change))   
-         # Write the second row
-    # csvwriter.writerow(['Caleb', 'Frost', '505-80-2901'])  
-0
+      
 
 
 
